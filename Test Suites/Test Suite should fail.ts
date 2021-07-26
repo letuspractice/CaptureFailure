193f9a04a50e0580c454d6d697b43823bd41d8b0
Thomas Kurtz <tkurtz@apu.edu>
2021-07-26 14:26:26 -0700

    initial commit

---
+++ Test Suites/Test Suite should fail.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite should fail</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9065467f-d57e-4d24-aa1f-1da0ca063083</testSuiteGuid>
+   <testCaseLink>
+      <guid>8fa7829e-1fb9-4836-84e6-61c567f2e2a7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 02 should fail</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
